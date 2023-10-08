9f119dcd4dd61e35a56043ae22d4b96be1bb94ba
Donka <DoniGeorgieva16@gmail.com>
2023-10-08 20:32:37 +0300

    Test Link Button UI

---
+++ src/components/ui/LinkButton/LinkButton.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import LinkButton from "./LinkButton";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+const MockedLinkButtonImplementation = () => {
+  return <LinkButton to="test">My text</LinkButton>;
+};
+describe("UI Link Button", () => {
+  it("should render without errors", async () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <MockedLinkButtonImplementation />
+      </MemoryRouter>
+    );
+    expect(getByText("My text")).toBeDefined();
+  });
+});
