b0becafe9736c8192e7d80ce00c7416cc0dfd0ff
Donka <DoniGeorgieva16@gmail.com>
2023-10-09 10:30:44 +0300

    Remove unused import

--- src/components/ui/LinkButton/LinkButton.test.tsx
+++ src/components/ui/LinkButton/LinkButton.test.tsx
@@ -1,6 +1,5 @@
 import { render } from "@testing-library/react";
 import LinkButton from "./LinkButton";
-import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 const MockedLinkButtonImplementation = () => {
   return <LinkButton to="test">My text</LinkButton>;
---
+++ src/pages/HomeScreen/HomeScreen.test.tsx
@@ -0,0 +1,5 @@
+describe("UI HomeScreen Component", () => {
+  it("Should render without errors", () => {
+    
+  });
+});
