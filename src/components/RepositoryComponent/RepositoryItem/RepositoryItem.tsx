d5b7c9179dbf55bf9af1e29fd59934ed24a6efeb
Donka <DoniGeorgieva16@gmail.com>
2023-10-17 13:48:57 +0300

    Remove repo columns on small screens

--- src/components/RepositoryComponent/RepositoryItem/RepositoryItem.tsx
+++ src/components/RepositoryComponent/RepositoryItem/RepositoryItem.tsx
@@ -11,16 +11,21 @@ const RepositoryItem: React.FunctionComponent<IFreeApi> = ({
 }: IFreeApi) => {
   return (
     <Card classNames="my-4 shadow-sm">
-      <article className="grid grid-cols-6 gap-2  p-4 break-words">
-        <h2 className="font-bold">{name}</h2>
+      <article className="grid grid-cols-3 md:grid-cols-6 gap-2  p-4 break-words">
+        <h2 className="font-bold ">{name}</h2>
         <p>{description}</p>
-        <p>{auth}</p>
-        <p data-testid="https">{https ? "Yes" : "No"}</p>
-        <p data-testid="cors">
+        <p className="hidden md:block">{auth}</p>
+        <p data-testid="https" className="hidden md:block">
+          {https ? "Yes" : "No"}
+        </p>
+        <p data-testid="cors" className="hidden md:block">
           {cors ? cors[0].toUpperCase() + cors.slice(1) : "No"}
         </p>
-        <a href={link} className="text-indigo-500 font-bold">
-          {link}
+        <a
+          href={link}
+          className="text-indigo-500 font-bold underline-offset-4 underline decoration-4 "
+        >
+          Link
         </a>
       </article>
     </Card>
