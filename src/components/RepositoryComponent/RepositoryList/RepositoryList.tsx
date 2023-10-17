93172a143a864927c7bddbae0002dba761594f1a
Donka <DoniGeorgieva16@gmail.com>
2023-10-17 13:36:10 +0300

    Add ScrollToTop Sticky Button

--- src/components/RepositoryComponent/RepositoryList/RepositoryList.tsx
+++ src/components/RepositoryComponent/RepositoryList/RepositoryList.tsx
@@ -3,6 +3,7 @@ import { IFreeApi } from "../../../interfaces/IFreeApi";
 import { mockedApis } from "../../../utils/mocked/mockedData/RepositoryComponent/mockedApis";
 import RepositoryItem from "../RepositoryItem/RepositoryItem";
 import RepositoryItemSkeleton from "../RepositoryItem/RepositoryItemSkeleton/RepositoryItemSkeleton";
+import StickyScrollButton from "../../ui/StickyButton/StickyButton";
 
 interface Props {
   data: IFreeApi[] | null;
@@ -12,27 +13,30 @@ interface Props {
 
 const RepositoryList = ({ data, isLoading, errorMessage }: Props) => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-2" data-testid="public-api-list">
-        Public APIs{" "}
-      </h1>
-      {isLoading && <h1>Loading</h1>}
-      <section className="grid grid-cols-6 gap-2 p-8 bg-white shadow-lg rounded-lg mt-5 ">
-        <h2 className="font-bold">Name</h2>
-        <h2 className="font-bold">Description</h2>
-        <h2 className="font-bold">Auth</h2>
-        <h2 className="font-bold">Https</h2>
-        <h2 className="font-bold">Cors</h2>
-        <h2 className="font-bold">Link</h2>
-      </section>
-      {!errorMessage && (
-        <List
-          data={data && data.length > 0 ? data : mockedApis}
-          keyPropertyName={["Link", "API"]}
-          itemComponent={!isLoading ? RepositoryItem : RepositoryItemSkeleton}
-        />
-      )}
-    </div>
+    <>
+      <div>
+        <h1 className="text-3xl font-bold mb-2" data-testid="public-api-list">
+          Public APIs{" "}
+        </h1>
+        {isLoading && <h1>Loading</h1>}
+        <section className="grid grid-cols-6 gap-2 p-8 bg-white shadow-lg rounded-lg mt-5 ">
+          <h2 className="font-bold">Name</h2>
+          <h2 className="font-bold">Description</h2>
+          <h2 className="font-bold">Auth</h2>
+          <h2 className="font-bold">Https</h2>
+          <h2 className="font-bold">Cors</h2>
+          <h2 className="font-bold">Link</h2>
+        </section>
+        {!errorMessage && (
+          <List
+            data={data && data.length > 0 ? data : mockedApis}
+            keyPropertyName={["Link", "API"]}
+            itemComponent={!isLoading ? RepositoryItem : RepositoryItemSkeleton}
+          />
+        )}
+      </div>
+      <StickyScrollButton />
+    </>
   );
 };
 
